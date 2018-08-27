8768f2cabee54aaf1724d4b44e836f76409b9a62
Moses Okocha <okocha.chukwutem@gmail.com>
2018-08-27 08:52:52 +0100

    Add connection to mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// DB config
+const db = require("./config/keys").mongoURL;
+
+// Connect to mongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("Database connection established"))
+  .catch(err => console.log("Error connecting to database: " + err));
 
 app.get("/", (req, res) => res.send('Home "/"'));
 
