f50014c04ac061e5f210e74e97e913fc1aa680fd
Moses Okocha <okocha.chukwutem@gmail.com>
2018-08-28 14:18:42 +0100

    Add GET "api/accounts/" route

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -1,5 +1,21 @@
 const router = require("express").Router();
+const passport = require("passport");
+const Account = require("../../models/Account");
 
-router.get("/test", (req, res) => res.json({ msg: "success" }));
+router.get("/test", (req, res) => res.json({ msg: success }));
 
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Account.find({ user: req.user._id }).then(accounts => {
+      if (!accounts) {
+        return res
+          .status(404)
+          .json({ user, error: "This user has no account" });
+      }
+      res.json({ user: req.user, accounts });
+    });
+  }
+);
 module.exports = router;
