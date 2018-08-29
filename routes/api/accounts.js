5091093fe764d1447f804f56eb21debfb4bae470
Moses Okocha <okocha.chukwutem@gmail.com>
2018-08-29 15:54:09 +0100

    Add POST "/api/accounts/:accountNumber/transfer" route

--- routes/api/accounts.js
+++ routes/api/accounts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
 const Account = require("../../models/Account");
+const Transaction = require("../../models/Transaction");
 
 router.get("/test", (req, res) => res.json({ msg: success }));
 
@@ -18,4 +19,41 @@ router.get(
     });
   }
 );
+
+router.post(
+  "/:accountNumber/transfer",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const accountNumber = req.params.accountNumber;
+
+    const transaction = new Transaction({
+      amount: req.body.amount,
+      type: req.body.type,
+      senderAccount: accountNumber,
+      recipientAccount: req.body.recipientAccount,
+      bankName: req.body.bankName,
+      description: req.body.description
+    });
+
+    // Add remarks for transaction
+    transaction.addRemark();
+
+    // Find current account
+    Account.findOne({ number: accountNumber })
+      .then(account => {
+        account.debitAccount(transaction.amount).then(() => {
+          transaction.save();
+        });
+        return res.json({ success: true, transaction });
+      })
+      .catch(err => {
+        console.log(err);
+        return res.status(400).json({
+          success: false,
+          transaction: err.message
+        });
+      });
+  }
+);
+
 module.exports = router;
